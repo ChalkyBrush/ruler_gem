2a54be87b20e3542761996d5a4cd02c10a57cd6a
rraciop <ryanracioppo@gmail.com>
2013-09-13 14:21:11 -0400

    Fixed terminal errors

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -6,12 +6,15 @@
       class Application
         def call(env)
             `echo debug > debug.txt`
+            if env['PATH_INFO'] == '/favicon.ico'
+              return [404,
+                {'Content-Type' => 'text/html'}, []]
             klass, act = get_controller_and_action(env)
             controller = klass.new(env)
             text = controller.send(act)
           [200, {'Content-Type' => 'text/html'},
 
-          ["Hello from Ruby on Rulers!"]]
+          [text]]
         end
 	   end 
 
